91e4dfc5fcb6406035bc9a81f3fb1df4e6bda8c0
va-nh <sakhnenko.ivan05@gmail.com>
2025-02-04 22:12:05 +0100

    our-story paralax effect

--- js/script.js
+++ js/script.js
@@ -8,6 +8,29 @@ burger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
 
+//паралакс ефект
+window.addEventListener('scroll', () => {
+    const section = document.querySelector('.page__our-story');
+    const background = document.querySelector('.our-story__background');
+    const content = document.querySelector('.our-story__container');
+
+    if (section) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const scrollY = window.scrollY;
+
+        // Перевіряємо, чи користувач прокручує в межах секції
+        if (scrollY + window.innerHeight > sectionTop && scrollY < sectionTop + sectionHeight) {
+            const scrollPosition = scrollY - sectionTop;
+
+            // Паралакс-ефект: фон рухається повільніше, контент швидше
+            background.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+            content.style.transform = `translateY(${scrollPosition * 0.1}px)`; // Зменшення швидкості контенту
+        }
+    }
+});
+
+
 
 
 // Функция для генерации случайного числа в диапазоне
